db13f9ba428cd82bd864d5a44e044177b3315dd3
vulasau <valiantsin.ulasau@gmail.com>
2025-11-17 16:54:51 +0100

    Fix step list in run script and correct file path for execution

--- src/run/run.R
+++ src/run/run.R
@@ -5,12 +5,12 @@ steps <- list(
   "2-b-values-clean.R",
   "3-b-values-transposed.R",
   "4-b-values-filtered.R",
-  "5-optional-step.R",
+  "5-b-values-merged.R",
   "6-pca.R",
-  "7pca-scores.R"
+  "7-pca-scores.R"
 )
 
-# Run each step sequentially in a separate R process with the arguments provided (args from common.R)
+# Run each step sequentially
 for (step in steps) {
-    system2("Rscript", args = c(file.path("src", "run", step), args))
+    system2("Rscript", args = c(step, args))
 }
