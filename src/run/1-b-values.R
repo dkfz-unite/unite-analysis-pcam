3365884d3804590df968ec77cf14908b14877860
vulasau <valiantsin.ulasau@gmail.com>
2025-11-28 16:27:50 +0100

    Corrected script to work with mix of meth array types

--- src/run/1-b-values.R
+++ src/run/1-b-values.R
@@ -1,39 +1,58 @@
 library(minfi)
-library("IlluminaHumanMethylationEPICmanifest", character.only = TRUE)
 source("common.R")
 
 cat("Step 1: Get B-values\n")
+
+preprocess_group <- function(files, method) {
+  red_grn_set <- read.metharray(files, extended = TRUE, force = TRUE)
+
+  if (method == "SWAN") {
+    b_set <- preprocessSWAN(red_grn_set)
+  } else if (method == "Quantile") {
+    b_set <- preprocessQuantile(red_grn_set)
+  } else if (method == "Noob") {
+    b_set <- preprocessNoob(red_grn_set)
+  } else if (method == "Raw") {
+    b_set <- preprocessRaw(red_grn_set)
+  } else {
+    b_set <- preprocessIllumina(red_grn_set)
+  }
+
+  rm(red_grn_set); gc()
+  getBeta(b_set)
+}
 
 # Read options
 options <- fromJSON(optionsFilePath)
 
 # Read metadata
 metadata <- read.table(file = inputFilePath, header = TRUE, sep = "\t", check.names = FALSE)
+files = metadata$path
 
-# Read IDATs
-red_grn_set <- read.metharray(metadata$path, extended = TRUE, force = TRUE)
+# Detect array types
+array_types <- sapply(files, function(f) { minfi::guessArrayType(f) })
+df <- data.frame(file = files, array = array_types, stringsAsFactors = FALSE)
 
-# Preprocess IDATs based on specified method
-method = options$pp
-if (method == "SWAN") {
-  b_set <- preprocessSWAN(red_grn_set)
-} else if (method == "Quantile") {
-  b_set <- preprocessQuantile(red_grn_set)
-} else if (method == "Noob") {
-  b_set <- preprocessNoob(red_grn_set)
-} else if (method == "Raw") {
-  b_set <- preprocessRaw(red_grn_set)
-} else {
-  b_set <- preprocessIllumina(red_grn_set)
+# Group by arrays type
+groups <- split(df$file, df$array)
+
+# Preprocess each group separately
+group_betas <- list()
+for (grp in names(groups)) {
+  group_betas[[grp]] <- preprocess_group(groups[[grp]], options$pp)
 }
 
-# Get B-values
-b_values <- getBeta(b_set)
+# Find common probes
+common_probes <- Reduce(intersect, lapply(group_betas, rownames))
 
-# Clean up
-rm(red_grn_set)
-rm(b_set)
-gc()
+# Merge B-values from all groups based on common probes
+merged <- do.call(cbind, lapply(group_betas, function(b) b[common_probes, , drop = FALSE]))
 
 # Dump step data
-saveRDS(b_values, file = "step-1.rds")
+saveRDS(merged, file = "step-1.rds")
+
+# Clean up
+rm(df);
+rm(group_betas);
+rm(merged);
+gc()
